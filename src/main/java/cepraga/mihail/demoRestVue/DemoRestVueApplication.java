b190b4a996e9a42244e2a47e673b463447d85c1d
mihail1986 <yahim09@gmail.com>
2019-04-03 23:04:06 +0300

    Initial commit

---
+++ src/main/java/cepraga/mihail/demoRestVue/DemoRestVueApplication.java
@@ -0,0 +1,13 @@
+package cepraga.mihail.demoRestVue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestVueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestVueApplication.class, args);
+	}
+
+}
