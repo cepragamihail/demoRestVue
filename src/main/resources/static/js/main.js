6828e2298864c0f2f5b51339aca6a6e8b835a575
mihail1986 <yahim09@gmail.com>
2019-04-04 21:16:22 +0300

    quite advance Vue component

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,11 +1,24 @@
-// Define a new component called todo-item
-Vue.component('todo-item', {
-  template: '<li>This is a todo</li>'
+// Define a new component called message-item
+Vue.component('message-item', {
+    props: ['message'],
+    template: '<li><i>({{ message.id }})</i> {{ message.text }}</li>'
+});
+// Define a new component called messages-list
+Vue.component('messages-list', {
+    props: ['messages'],
+    template: '<ol><message-item v-for="message in messages" :key="message.id" :message="message" /></ol>'
 });
 
 var app = new Vue({
   el: '#app',
+  template: '<messages-list :messages="messages" />',
   data: {
-    message: 'Hello Vue!'
+    messages: [
+    {id: '1', text: 'message 1'},
+    {id: '2', text: 'message 2'},
+    {id: '3', text: 'message 3'},
+    {id: '4', text: 'message 4'},
+    {id: '5', text: 'message 5'}
+    ]
   }
 });
